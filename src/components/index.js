bdf3c13c1d36ecbbaa788a779326c4545565c87a
Marcos Sierra <marcosjspac@gmail.com>
2022-01-18 17:30:12 -0600

    creadno proyecto base

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function mainPage(){
+    return(
+        <React.Fragment>
+            <h1>Portal Web</h1>
+        </React.Fragment>
+    );
+}
+
+export { mainPage }
