7e6568a6239145f7a7ec8cbd86d799a3edbec92a
Marcos Sierra <marcosjspac@gmail.com>
2022-01-19 12:28:31 -0600

    arreglando propiedades de componentes

--- src/components/main/tarjeta.js
+++ src/components/main/tarjeta.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 function TarjetaMain(props) {
   return(
     <React.Fragment>
-      <div class="col-sm-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">{props.titulo}</h5>
-            <p class="card-text">
+      <div className="col-sm-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{props.titulo}</h5>
+            <p className="card-text">
               Una pagina para consultar e interactuar con información de {props.texto}.
             </p>
-            <Link to={props.ruta} class="btn btn-primary">
+            <Link to={props.ruta} className="btn btn-primary">
               {props.textoBoton}
             </Link>
           </div>
